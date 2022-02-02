9e68a313c33282c9785fc5e289ca65248af08ca3
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2022-02-02 15:10:47 -0600

    api-commerceJs-wip7

--- WebAPI/APICommerceJs/Controllers/CommerceJsController.cs
+++ WebAPI/APICommerceJs/Controllers/CommerceJsController.cs
@@ -12,7 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace api_job_apps.Controllers
+namespace APICommerceJs.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
